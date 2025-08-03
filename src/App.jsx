2afbc48e42ffe451d94fa61c3b61a497f2530061
masumparvez56 <parvezmasum581@gmail.com>
2025-08-03 19:38:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='font-poppins'>
+       <h1 className='text-3xl p-2 font-bold'>Hi! I am Developer</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
